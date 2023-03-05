8506a84741f02d3a0e25cb544a091b481a67df2e
Quang Vu Tran <quangvutran.1994@gmail.com>
2023-03-04 19:26:17 -0700

    project week 8

--- notebooks/flask_request.py
+++ notebooks/flask_request.py
@@ -1,47 +1,24 @@
-from flask import Flask, jsonify, Request
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import numpy
 import pickle
-
 
 app = Flask(__name__)
 api = Api(app)
 
-# import models
+model = pickle.load( open( "model.pkl", "rb" ))
 
-with open('model_columns.pkl', 'rb') as f:
-   model_columns = pickle.load (f)
+class Test(Resource):
+    def post(self):
+        json_data = request.get_json()
+        df = pd.DataFrame(json_data.values(), index=json_data.keys()).transpose()
+        # to have a result is probability
+        # res = model.predict_proba(df)
+        res = model.predict(df)
+        return res.tolist() 
 
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
-# web page
-@app.route('/')
-def welcome():
-   return "Welcome! Use this Flask App for Loan Approval Prediction"
+api.add_resource(Test, '/test')
 
-@app.route('/predict', methods=['POST','GET'])
-def predict():
-
-   if flask.request.method == 'GET':
-       return "Prediction page. Try using post with params to get specific prediction."
-
-   if flask.request.method == 'POST':
-       try:
-           json_ = request.json # '_' since 'json' is a special word
-           print(json_)
-           query_ = pd.get_dummies(pd.DataFrame(json_))
-           query = query_.reindex(columns = model_columns, fill_value= 0)
-           prediction = list(model.predict(query))
-
-           return jsonify({
-               "prediction":str(prediction)
-           })
-
-       except:
-           return jsonify({
-               "trace": traceback.format_exc()
-               })
-
-
-
-if __name__ == "__main__":
-   app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
--- notebooks/test.py
+++ notebooks/test.py
@@ -3,28 +3,27 @@ import requests as r
 import pandas as pd
 import json
 
-base_url =  'http://127.0.0.1:5000/' #base url local host
+base_url =  'http://127.0.0.1:5000/test' #base url local host
 
-json_data = [
-    {
+json_data = {
     "Gender" : "Male",
-    "Married" : "No",
-    "Dependents" : "2",
+    "Married" : "Yes",
+    "Dependents" : "1",
     "Education" : "Graduate",
     "Self_Employed" : "No",
     "ApplicantIncome" : 5849,
-    "CoapplicantIncome" : 0,
+    "CoapplicantIncome" : 1000,
     "LoanAmount" : 400,
     "Loan_Amount_Term" : 360,
     "Credit_History" : 1.0,
     "Property_Area" : "Urban"
     }
-]
+
 
 # Get Response
 # response = r.get(base_url)
-response = r.post(base_url + "predict", json = json_data)
-
+response = r.post(base_url, json = json_data)
+print(response.status_code)
 
 if response.status_code == 200:
     print('...')
